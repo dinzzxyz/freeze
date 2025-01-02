029c1ccaf05701db01e09578f82b5c4eb812310b
dinzzxyz <dinarzakariyya@gmail.com>
2025-01-02 13:45:58 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -143,7 +143,6 @@ local function createLoadingGUI(duration, onComplete)
     loadingCircle.BackgroundColor3 = Color3.new(1, 1, 1) -- Warna putih
     loadingCircle.Size = UDim2.new(0.1, 0, 0.1, 0)
     loadingCircle.Position = UDim2.new(0, 10, 0, 110)
-    loadingCircle.AnchorPoint = Vector2.new(0.5, 0.5)
 
     local rotation = 0
     game:GetService("RunService").RenderStepped:Connect(function()
